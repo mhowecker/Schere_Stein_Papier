c6f8d56468c27ea926455db53729d754da9fc8d2
Martin Howecker <mhowecker@student.tgm.ac.at>
2022-09-21 19:22:25 +0200

    2. Variante: SpinBox durch ComboBox ersetzt

--- view.py
+++ view.py
@@ -7,7 +7,12 @@ class View(QMainWindow):
 
     # Klassenvariablen
 
+    # GUI1 - Spinbox
     spB_sign: QSpinBox
+
+    # GUI2 - ComboBox
+    coB_sign: QComboBox
+
     l_round_number: QLabel
     l_player_points: QLabel
     l_computer_points: QLabel
@@ -19,11 +24,17 @@ class View(QMainWindow):
     def __init__(self, c: Controller):
         super().__init__()
 
-        uic.loadUi("Schere_Stein_Papier.ui", self)
+        uic.loadUi("Schere_Stein_Papier2.ui", self)
 
-        self.spB_sign.setValue(1)
-        self.spB_sign.setMinimum(1)
-        self.spB_sign.setMaximum(3)
+        # GUI1 mit SpinBox
+        # self.spB_sign.setValue(1)
+        # self.spB_sign.setMinimum(1)
+        # self.spB_sign.setMaximum(3)
+
+        # GUI2 mit ComboBox
+        self.coB_sign.addItem("Schere")
+        self.coB_sign.addItem("Stein")
+        self.coB_sign.addItem("Papier")
 
         self.pb_reset.clicked.connect(c.reset)
         self.pb_execute.clicked.connect(c.execute)
@@ -33,10 +44,18 @@ class View(QMainWindow):
         self.l_turn_last.setText("letzter Zug")
         self.l_player_points.setText("0")
         self.l_computer_points.setText("0")
-        self.spB_sign.setValue(1)
+        # GUI1 - SpinBox
+        # self.spB_sign.setValue(1)
+
+        # GUI2 - ComboBox
+        self.coB_sign.setCurrentIndex(0)
 
     def get_sign(self) -> int:
-        return self.spB_sign.value()
+        # GUI1 - SpinBox
+        # return self.spB_sign.value()
+
+        # GUI2 - ComboBox
+        return self.coB_sign.currentIndex()+1
 
     def update(self, p1: int, p2: int, rounds: int, last: str) -> None:
         self.l_player_points.setText(str(p1))
